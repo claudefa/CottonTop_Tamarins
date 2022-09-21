66f84345f2b793854bc6f9a9c8cd877c040fce1d
qvw641 <qvw641@mjolnirhead01fl.unicph.domain>
2022-09-21 13:44:57 +0200

    new cluster

--- cluster_jobs/snpAD.sh
+++ cluster_jobs/snpAD.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set directories
-DIR="/scratch/devel/cfontser/CTT/"
-IN=${DIR}"BAMs/"
+DIR="/projects/mjolnir1/people/qvw641/CottonTop/"
+IN=${DIR}/"BAMs/"
 OUTDIR=${DIR}"VCF/"
 
 mkdir -p $OUTDIR
@@ -18,30 +18,34 @@ mkdir -p $out
 # Reference
 
 #REF
-ASSEMBLY="/scratch/devel/cfontser/CTT/Assembly/SaguinusMidas_NCBI.fasta"
-
-module purge;module load gcc/6.3.0 popt/1.16 nlopt/2.5.0 R/3.5.0 snpad PYTHON/3.6.3 tabix
+ASSEMBLY="/projects/mjolnir1/people/vbz170/projects/CTT/Ref_Genome/Saguinus_midas_Full_Genome/NCBI_version/SaguinusMidas_NCBI.fasta"
 
 while read chrom;
 do
-while read line;
-do
-        sample=`echo $line | awk '{print $1}'`
+# send array
+array=$qu/SnpAD_${chrom}_array.sh
 
-        bam=${IN}${sample}.SagMidas.realigned.bam
-	snpad=${OUTDIR}/${chrom}/${sample}_${chrom}.snpAD
-	vcf=${OUTDIR}/${chrom}/${sample}_${chrom}.vcf
-	mkdir -p ${OUTDIR}/${chrom}/
-        jobName=${qu}/2VCF.${sample}_${chrom}.sh
+echo "#!/bin/bash" > $array
+echo "#" >> $array
+echo "#SBATCH -c 8" >> $array
+echo "#SBATCH --mem-per-cpu 200G" >> $array
+echo "#SBATCH --time=30:00:00"  >> $array
+echo "#SBATCH --array=1-35%10" >> $array
+echo "#SBATCH --output=${out}/snpad.${chrom}.%A_%a.log" >> $array
+echo "#SBATCH --job-name snpad.${chrom}" >> $array
 
-	echo "Bam2snpAD -r $chrom -f $ASSEMBLY -Q 30 $bam  > $snpad; \
-		snpAD -c 12 -o ${OUTDIR}/${chrom}/${sample}.priors.txt -O ${OUTDIR}/${chrom}/${sample}.errors.txt $snpad ; \
-		snpADCall -N 1 -e ${OUTDIR}/${chrom}/${sample}.errors.txt -p \"\`cat ${OUTDIR}/${chrom}/${sample}.priors.txt\`\" $snpad > $vcf; \
-		bgzip $vcf; tabix -p vcf ${vcf}.gz; rm $snpad; rm ${OUTDIR}/${chrom}/${sample}.priors.txt; rm ${OUTDIR}/${chrom}/${sample}.errors.txt" > ${jobName}
-        chmod 755 $jobName
-        echo $jobName
-	python3 ~/submit.py  -c $jobName -n vcf${sample} -o ${out}/vcf${sample}_${chrom}.out -e  ${out}/vcf${sample}_${chrom}.err -u 8 -r lowprio -w "28:59:00"
+echo "conda activate /projects/mjolnir1/apps/conda/picard-2.15.0/" >> $array
+echo "sample=\$(sed -n \"\$SLURM_ARRAY_TASK_ID\"p ${DIR}/Samples | awk '{print \$1}')" >> $array
+echo "echo \$sample" >> $array
+echo "bam=${IN}\${sample}.SagMidas.realigned.bam" >> $array
+echo "snpad=${OUTDIR}/${chrom}/\${sample}_${chrom}.snpAD" >> $array
+echo "mkdir -p ${OUTDIR}/${chrom}/" >> $array
+echo "module load snpAD/0.3.10" >> $array
+echo "Bam2snpAD -r $chrom -f $ASSEMBLY -Q 30 \$bam  > \$snpad; \
+      snpAD -c 12 -o ${OUTDIR}/${chrom}/\${sample}.priors.txt -O ${OUTDIR}/${chrom}/\${sample}.errors.txt \$snpad ; \
+      snpADCall -N 1 -e ${OUTDIR}/${chrom}/\${sample}.errors.txt -p \"\`cat ${OUTDIR}/${chrom}/\${sample}.priors.txt\`\" \$snpad > \$vcf; \
+      bgzip \$vcf; tabix -p vcf \${vcf}.gz; rm \$snpad; rm ${OUTDIR}/${chrom}/\${sample}.priors.txt; rm ${OUTDIR}/${chrom}/\${sample}.errors.txt" >> ${array}
 
-done < Samples
 
-done < Chrom_test
+#sbatch $array
+done < Chrom_autosomes
