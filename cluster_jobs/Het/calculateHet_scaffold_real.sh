5e1ec92db1781b3526aa26c55963e31e9c2a914b
qvw641 <qvw641@mjolnirhead01fl.unicph.domain>
2022-10-28 15:32:20 +0200

    heterozygosity

--- cluster_jobs/Het/calculateHet_scaffold_real.sh
+++ cluster_jobs/Het/calculateHet_scaffold_real.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 sample=$1;
-if [ -z "$sample" ];then printf "### Calculate Het by scaffold ###\n\nUSAGE:\n\nbash calculateHet_scaffold.sh Sample  \n\n";exit;fi
+id=$2
+if [ -z "$sample" ];then printf "### Calculate Het by scaffold ###\n\nUSAGE:\n\nbash calculateHet_scaffold.sh Sample ID_vcf \n\n";exit;fi
 
-dir="/scratch/devel/cfontser/CTT/VCF/Heterozygosity"
+dir=/projects/mjolnir1/people/qvw641/CottonTop/VCF/Het/
 het=${dir}/Het_real/${sample}/
 out=${dir}/Het_real/out/
 qu=${dir}/Het_real/qu/
@@ -16,18 +17,18 @@ do
 	region=$(echo $line | awk '{print $1":"$2"-"$3}');
 	read chrom start end <<<$(echo $line);
         jobName=${qu}/Scaffold_${chrom}_${sample}.sh
-	vcf="/scratch/devel/cfontser/CTT/VCF/MergeVCFs/CTT_${chrom}.g.vcf.gz"
+	vcf="/projects/mjolnir1/people/qvw641/CottonTop/VCF/MergeVCFs/CTT_${chrom}.g.vcf.gz"
 
 	echo "#!/bin/bash" > ${jobName} 
-	echo "callable=\$(tabix -h $vcf $region | vcftools --vcf - --indv $sample --recode --max-missing 1 --recode-INFO-all --stdout --minDP 3 --maxDP 30 --minQ 30 | grep -v '#' | wc -l ) " >> ${jobName}
+	echo "callable=\$(tabix -h $vcf $region | vcftools --vcf - --indv $id --recode --max-missing 1 --recode-INFO-all --stdout --minDP 3 --maxDP 30 --minQ 30 | grep -v '#' | wc -l ) " >> ${jobName}
 	echo "if [ \$callable -gt 0 ];">> ${jobName}
 	echo "then" >> ${jobName}
-	echo "	tabix -h $vcf $region | vcftools --vcf - --indv $sample --recode --recode-INFO-all --max-missing 1 --stdout --minDP 3 --maxDP 30 --minQ 30 | vcfhetcount | tail -n 1 | awk -v l=\$callable -v chrom=$chrom '{print chrom\"\t\"\$1\"\t\"l\"\t\"\$1/l}' > ${het}/${sample}_${chrom}.het" >> ${jobName}
+	echo "	tabix -h $vcf $region | vcftools --vcf - --indv $id --recode --recode-INFO-all --max-missing 1 --stdout --minDP 3 --maxDP 30 --minQ 30 | grep 0/1 | wc -l | awk -v l=\$callable -v chrom=$chrom '{print chrom\"\t\"\$1\"\t\"l\"\t\"\$1/l}' > ${het}/${sample}_${chrom}.het" >> ${jobName}
 	echo "else" >> ${jobName}
 	echo "	echo $line | awk -v l=0 -v het=NA '{print \$1\"\t\"l\"\t\"l\"\t\"het}' > ${het}/${sample}_${chrom}.het" >> ${jobName}
 	echo "fi" >> ${jobName}
 	echo "callable=''" >> ${jobName} 
 	chmod 755 $jobName
-	python3 ~/submit.py  -u 1 -c $jobName -n Het_${sample} -o ${out}/Scaff_${sample}.out -e ${out}/Scaff${sample}.err -w "10:00:00"
+	sbatch -c 1 --mem-per-cpu 1G --time 8:00:00 -o ${out}/Het_${chrom}_${sample}.log --job-name Het_${sample}_$chrom -- $jobName
 
-done < <(tail -n 21 Windows/Assembly_scaffolds_autosomes.bed)
+done < <(tail -n 21 /home/qvw641/CottonTop_Tamarins/cluster_jobs/Het/Windows/Assembly_scaffolds_autosomes.bed)
--- cluster_jobs/Het/calculateHet_windows_real.sh
+++ cluster_jobs/Het/calculateHet_windows_real.sh
@@ -2,20 +2,21 @@
 
 bed=$1;
 sample=$2;
-if [ -z "$bed" ] || [ -z "$sample" ];then printf "### Calculate Het by windows ###\n\nUSAGE:\n\nbash calculateHet_windows.sh 100000 Sample \n\n";exit;fi
-mkdir -p Het_windows_real/${sample}/
+id=$3
+if [ -z "$bed" ] || [ -z "$sample" ] || [ -z "$id" ] ;then printf "### Calculate Het by windows ###\n\nUSAGE:\n\nbash calculateHet_windows.sh 100000 Sample ID_vcf\n\n";exit;fi
+mkdir -p /projects/mjolnir1/people/qvw641/CottonTop/VCF/Het/Het_windows_real/${sample}/
 
 while read line;
 do 
 	region=$(echo $line | awk '{print $1":"$2"-"$3}');
 	read chrom start end <<<$(echo $line);
-	vcf="/scratch/devel/cfontser/CTT/VCF/MergeVCFs/CTT_${chrom}.g.vcf.gz"
-	callable=$(tabix -h $vcf $region | vcftools --vcf - --indv $sample --max-missing 1 --stdout --recode --recode-INFO-all --minDP 3 --maxDP 30 --minQ 30 | grep -v '#' | wc -l ) 
+	vcf="/projects/mjolnir1/people/qvw641/CottonTop/VCF/MergeVCFs/CTT_${chrom}.g.vcf.gz"
+	callable=$(tabix -h $vcf $region | vcftools --vcf - --indv $id --max-missing 1 --stdout --recode --recode-INFO-all --minDP 3 --maxDP 30 --minQ 30 | grep -v '#' | wc -l ) 
 	if [ $callable -gt 0 ];
 	then
-		tabix -h $vcf $region | vcftools --vcf - --indv $sample --max-missing 1 --stdout --recode --recode-INFO-all --minDP 3 --maxDP 30 --minQ 30 | vcfhetcount | tail -n 1 | awk -v l="$callable" -v chrom="$chrom" -v start="$start" '{print chrom"\t"start+50000"\t"$1"\t"l"\t"$1/l}'
+		tabix -h $vcf $region | vcftools --vcf - --indv $id --max-missing 1 --stdout --recode --recode-INFO-all --minDP 3 --maxDP 30 --minQ 30 | grep 0/1 | wc -l  | awk -v l="$callable" -v chrom="$chrom" -v start="$start" '{print chrom"\t"start+50000"\t"$1"\t"l"\t"$1/l}'
 	else
 		echo $line | awk -v l=0 -v het=NA '{print $1"\t"$2+50000"\t"l"\t"l"\t"het}'
 	fi
 	callable=''
-done <  Windows/${bed}.bed > Het_windows_real/${sample}/${sample}_${bed##*/}_wind.het
+done <  /home/qvw641/CottonTop_Tamarins/cluster_jobs/Het/Windows/${bed}.bed > /projects/mjolnir1/people/qvw641/CottonTop/VCF/Het/Het_windows_real/${sample}/${sample}_${bed##*/}_wind.het
