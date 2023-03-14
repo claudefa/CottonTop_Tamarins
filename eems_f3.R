5d7e61f4b9124a409221632647c5e4429c775358
Claudia Fontsere <qvw641@sun1020626.unicph.domain>
2023-03-14 11:47:39 +0100

    eems

--- eems_f3.R
+++ eems_f3.R
@@ -1,28 +1,41 @@
-# EEMS, F3 and maps
+# EEMS and F3
 library(reemsplots2)
-#library(rEEMSplots)
 library(rgdal)
 library(rworldmap)
 library(rworldxtra)
 library(RColorBrewer)
 library(ggplot2)
-library("tidyr")
+library(tidyr)
 library(corrplot)
 
+library('maps')
+library('mapdata')
+library('maptools')
+library("ggplot2")
+library("readxl")
+library("rgdal") 
+library(svglite)
+library("tidyr")
+library("ggConvexHull")
+library("scales")
+library("ggrepel")
+library("RColorBrewer")
+library("viridis")
+library("ggpubr")
+library("cowplot")
+
 setwd("~/Documents/OneDrive - University of Copenhagen/CottonTop_Tamarins/")
-metadata  <- read_excel("Shotgun_Metadata.xlsx")
+metadata  <- read_excel("Paper/SupplementaryTables_v1.1.xlsx", sheet = 4)
 
 # EEMS CTT ------
-
 # map specs
+map <-readOGR("Files/ne_50m_admin_0_countries/","ne_50m_admin_0_countries")
 
-map <-readOGR("~/Documents/OneDrive - University of Copenhagen/CottonTop_Tamarins/Files/ne_50m_admin_0_countries/","ne_50m_admin_0_countries")
-# all sites
-for (i in 1:10){
+#for (i in 1:10){
   i=1
   print(i)
   mcmcpath = paste0("./Files/CTT-EEMS-nDemes1000-chain",i,"/")
-  plotpath = paste0("./Plots/NewIter_", i,"/")
+  plotpath = paste0("./Plots/NewIter_", i,"_2/")
   dir.create(plotpath)
   
   projection_none <- "+proj=longlat +datum=WGS84" 
@@ -37,15 +50,6 @@ for (i in 1:10){
                   add_outline = FALSE, col_outline = "#FFFFFF", eems_colors = NULL,
                   prob_level = 0.9, m_colscale = NULL, q_colscale = NULL,
                   add_abline = FALSE)
-  
-  eems.plots(mcmcpath, plotpath, longlat = TRUE, 
-             add.grid=FALSE, add.outline = TRUE, add.demes = TRUE,
-             add.map = TRUE,projection.in = projection_none,
-             projection.out = projection_mercator,
-             m.plot.xy = { plot(map_africa, col = NA, add = TRUE) },
-             q.plot.xy = { plot(map_africa, col = NA, add = TRUE) },
-             eems.colors = colorRampPalette(brewer.pal(11, "RdBu"))(200),
-             out.png=FALSE, min.cex.demes = 0.5, max.cex.demes = 1.5 )
   
  new_plots <-plots$mrates01 +
    geom_path(data = map,aes(x = long,y = lat,group = group),colour = "black",size=0.4) + 
@@ -66,50 +70,15 @@ for (i in 1:10){
  ggsave(paste0(plotpath, "mrates02.pdf"), new_plots2, width = 6, height = 4) 
 
   ggarrange(new_plots, new_plots2, labels = c("A", "B"), widths = c(1,1.255))
-  ggsave("Plots/EEMS_final.pdf", width = 8, height = 4) 
+  ggsave("Plots/Figure2.pdf", width = 8, height = 4) 
+# } 
   
- } 
-  
-  
-  
-
 # F3--------------
 #F3
-#onlyCTT  
 f3dat_sites <- read.table("Files/all_sites_onlyCTT_final.qp3Pop.out",
                             col.names=c("PopA", "PopB", "Outgroup", "F3", "StdErr", "Z", "SNPs"))
 f3dat_sites$low <- f3dat_sites$F3 - f3dat_sites$StdErr
 f3dat_sites$high <- f3dat_sites$F3 + f3dat_sites$StdErr
-
-
-
-
-f3<- ggplot(f3dat_sites_final, aes(PopA, F3))
-f3 <- f3 + geom_point() +   
-  #scale_fill_gradientn(colours = colorRampPalette(c(brewer.pal( 11, "RdYlBu")))(200)) +   
-  ylab("") + xlab("")+
-  theme(axis.text.x = element_text(angle=90, hjust=1))
-f3
-
-# spread
-colnames(f3dat_sites_final) <- c("PopA","PopB","F3")
-x <- spread(f3dat_sites_final, PopA, F3, fill=NA,convert = FALSE)
-names <- x$PopB
-x <- x[,2:ncol(x)]
-rownames(x) <- names
-y <- data.matrix(x)
-y
-
-pdf("Plots/F3_sites_onlyCTT_newVCF.pdf", height=8, width = 10 )
-corrplot(y,addrect = 4, na.label = "NA",
-         tl.col = "black", type="full",order="hclust",na.label.col="white",
-         tl.cex = 0.95,  
-         tl.srt = 90, 
-         rect.col="black",
-         pch.cex = 1.0, method="color",is.corr = FALSE, col=colorRampPalette(c("#f7f7f7","#d1e5f0","#92c5de","#4393c3","#2166ac","#053061"))(200),
-         pch.col = "purple")
-
-dev.off()
 
 # only ones with coordinates
 order <- c("Tulenapa_Modern", "Mutata_Historical", "Turbo_Historical",
@@ -131,67 +100,18 @@ f3<- ggplot(f3dat_sites_final, aes(PopA, PopB, fill=F3))
 f3 <- f3 + geom_tile() +   scale_fill_viridis_c()+
   #scale_fill_gradientn(colours = colorRampPalette(c(brewer.pal( 11, "RdYlBu")))(200)) +   
   ylab("") + xlab("")+ ggtitle("f3(S.midas;PopA,PopB)")+ theme_classic()+
-  theme(axis.text.x = element_text(angle=90, hjust=1), axis.line = element_blank())
+  theme(axis.text.x = element_text(angle=45, hjust=1), axis.line = element_blank())
 f3
-pdf("Plots/F3_onlyGPS.pdf", height = 8, width = 10)
-f3
-dev.off()
-
-
-
-
-#only historical 
-historical <- c("Arjona_Historical","Caracas_Historical","Cauca_Historical",
-                "Coloso_Historical","Mutata_Historical",
-                "PlanetaRica_Historical","SanJuan_Historical",
-                "Tierralta_Historical","Turbo_Historical","Unknown_Historical")
-f3dat_sites_final_hist <- f3dat_sites_final[which(f3dat_sites_final$PopA%in%historical & f3dat_sites_final$PopB%in% historical),]
-
-colnames(f3dat_sites_final_hist) <- c("PopA","PopB","F3")
-x <- spread(f3dat_sites_final_hist, PopA, F3, fill=NA,convert = FALSE)
-names <- x$PopB
-x <- x[,2:ncol(x)]
-rownames(x) <- names
-y <- data.matrix(x)
-y
-
-pdf("Plots/F3_sites_onlyCTT_newVCF_historical.pdf", height=8, width = 10 )
-corrplot(y,addrect = 3, na.label = "NA",
-         tl.col = "black", type="full",order="hclust",na.label.col="white",
-         tl.cex = 0.95,  
-         tl.srt = 90, 
-         rect.col="black",
-         pch.cex = 1.0, method="color",is.corr = FALSE, 
-         col=colorRampPalette(c("#f7f7f7","#d1e5f0","#92c5de","#4393c3","#2166ac","#053061"))(200),
-         pch.col = "purple")
-
-dev.off()
-
 
 
 # Map per population only CTT all with coordinates-----
-library('maps')
-library('mapdata')
-library('maptools')
-library("ggplot2")
-library("readxl")
-library("rgdal") 
-library(svglite)
-library("tidyr")
-library("ggConvexHull")
-library("scales")
-library("ggrepel")
-library("RColorBrewer")
-library("viridis")
-library("ggpubr")
-library("cowplot")
 # Construct Map
-map <-readOGR("~/Documents/OneDrive - University of Copenhagen/CottonTop_Tamarins/Files/ne_50m_admin_0_countries/","ne_50m_admin_0_countries")
-rivers <-readOGR("~/Documents/OneDrive - University of Copenhagen/CottonTop_Tamarins/Files/ne_110m_rivers_lake_centerlines/ne_110m_rivers_lake_centerlines.shp")
-ocean<-readOGR("~/Documents/OneDrive - University of Copenhagen/CottonTop_Tamarins/Files/ne_10m_ocean/ne_10m_ocean.shp")
+map <-readOGR("Files/ne_50m_admin_0_countries/","ne_50m_admin_0_countries")
+rivers <-readOGR("Files/ne_110m_rivers_lake_centerlines/ne_110m_rivers_lake_centerlines.shp")
+ocean<-readOGR("Files/ne_10m_ocean/ne_10m_ocean.shp")
 
 # CTT
-area.1 <- readOGR("~/Documents/OneDrive - University of Copenhagen/CottonTop_Tamarins/Files/redlist_species_data_94c6c081-f927-416b-8ea9-ef72a2300dc4/")
+area.1 <- readOGR("Files/redlist_species_data_94c6c081-f927-416b-8ea9-ef72a2300dc4/")
 area.1.points <- fortify(area.1)
 area.all <- rbind.data.frame(area.1.points)
 
@@ -204,7 +124,7 @@ k <- k +  geom_polygon(data = map, aes(x = long,y = lat, group = group),fill = "
   theme(panel.background = element_rect(fill = '#e3f0ff'),panel.grid.major = element_blank(),panel.grid.minor = element_blank())
 
 k
-# Subspecies Map
+
 area.all$species <- area.all$group
 colors_habitat=rep(c("#f2cc9b"),  times=c(15))
 
@@ -237,7 +157,7 @@ p <- p + geom_polygon(data = area.all,aes(x = long,y = lat, fill = group), color
         legend.background = element_blank(),
         legend.key=element_rect(fill="white"),
         legend.box = "horizontal") +
-  ggtitle(paste0("f3(S.midas;X;",site,")"))
+  ggtitle(paste0("f3(S.midas;X,",site,")"))
 p
 
 pdf(paste0("Plots/Map_f3_",site,".pdf"), width = 6, height = 5)
@@ -268,7 +188,7 @@ p <- p + geom_polygon(data = area.all,aes(x = long,y = lat, fill = group), color
         legend.background = element_blank(),
         legend.key=element_rect(fill="white"),
         legend.box = "horizontal") +
-  ggtitle(paste0("f3(S.midas;X;",site,")"))
+  ggtitle(paste0("f3(S.midas;X,",site,")"))
 p
 
 i=8
@@ -294,92 +214,10 @@ q <- q + geom_polygon(data = area.all,aes(x = long,y = lat, fill = group), color
         legend.background = element_blank(),
         legend.key=element_rect(fill="white"),
         legend.box = "horizontal") +
-  ggtitle(paste0("f3(S.midas;X;",site,")"))
+  ggtitle(paste0("f3(S.midas;X,",site,")"))
 q
 
-ggarrange(p,q,ncol=2)
-ggsave("Plots/F3_maps.pdf", width = 10, height = 6)
-
-# with S. geoffroyi
-f3dat_sites <- read.table("Files/all_sites_newvcf.qp3Pop.out",
-                          col.names=c("PopA", "PopB", "Outgroup", "F3", "StdErr", "Z", "SNPs"))
-
-f3dat_sites$low <- f3dat_sites$F3 - f3dat_sites$StdErr
-f3dat_sites$high <- f3dat_sites$F3 + f3dat_sites$StdErr
-
-f3dat_sites_final <- cbind.data.frame(f3dat_sites$PopA,f3dat_sites$PopB, f3dat_sites$F3)
-colnames(f3dat_sites_final) <- c("PopA","PopB","F3")
-
-f3<- ggplot(f3dat_sites_final, aes(PopA, PopB, fill=F3))
-f3 <- f3 + geom_tile() +   scale_fill_viridis_c()+
-  #scale_fill_gradientn(colours = colorRampPalette(c(brewer.pal( 11, "RdYlBu")))(200)) +   
-  ylab("") + xlab("")+
-  theme(axis.text.x = element_text(angle=90, hjust=1))
-f3
-
-# spread
-colnames(f3dat_sites_final) <- c("PopA","PopB","F3")
-x <- spread(f3dat_sites_final, PopA, F3, fill=NA,convert = FALSE)
-names <- x$PopB
-x <- x[,2:ncol(x)]
-rownames(x) <- names
-y <- data.matrix(x)
-y
-
-pdf("Plots/F3_sites_allSamples_newVCF.pdf", height=8, width = 10 )
-corrplot(y,addrect = 4, na.label = "NA",
-         tl.col = "black", type="full",order="hclust",na.label.col="white",
-         tl.cex = 0.95,  
-         tl.srt = 90, 
-         rect.col="black",
-         pch.cex = 1.0, method="color",is.corr = FALSE, col=colorRampPalette(c("#f7f7f7","#d1e5f0","#92c5de","#4393c3","#2166ac","#053061"))(200),
-         pch.col = "purple")
-
-dev.off()
-
-# F3 per sample
-f3dat_indv <- read.table("Files/allIndv.qp3Pop.out",
-                          col.names=c("PopA", "PopB", "Outgroup", "F3", "StdErr", "Z", "SNPs"))
-
-f3dat_indv$low <- f3dat_indv$F3 - f3dat_indv$StdErr
-f3dat_indv$high <- f3dat_indv$F3 + f3dat_indv$StdErr
-
-metadata$PopB <- metadata$Full_ID
-f3dat_indv <- merge(f3dat_indv, metadata[ , c("Site","PopB","Coverage","Type")], by="PopB")                            
-
-
-# spread
-f3dat_sites_final <- cbind.data.frame(f3dat_indv$PopA,f3dat_indv$PopB, f3dat_indv$F3)
-colnames(f3dat_sites_final) <- c("PopA","PopB","F3")
-colnames(f3dat_sites_final) <- c("PopA","PopB","F3")
-x <- spread(f3dat_sites_final, PopA, F3, fill=NA,convert = FALSE)
-names <- x$PopB
-x <- x[,2:ncol(x)]
-rownames(x) <- names
-y <- data.matrix(x)
-
-corrplot(y,addrect = 4, na.label = "NA",
-         tl.col = "black", type="full",order="hclust",na.label.col="white",
-         tl.cex = 0.95,  
-         tl.srt = 90, 
-         rect.col="black",
-         pch.cex = 1.0, method="color",is.corr = FALSE, col=colorRampPalette(c("#f7f7f7","#d1e5f0","#92c5de","#4393c3","#2166ac","#053061"))(200),
-         pch.col = "purple")
-
-
-f3<- ggplot(f3dat_indv[f3dat_indv$PopB=="36_AMNH_32702",], aes(PopA, F3, fill=F3))
-f3 <- f3 + geom_point() + geom_errorbar(aes(ymin=low, ymax=high), width=.2,
-                                          position=position_dodge(0.05))  +
-  scale_fill_viridis_c()+ facet_wrap(.~PopB)+
-  #scale_fill_gradientn(colours = colorRampPalette(c(brewer.pal( 11, "RdYlBu")))(200)) +   
-  ylab("") + xlab("")+
-  theme(axis.text.x = element_text(angle=90, hjust=1))
-f3
-
-
-cor.test(f3dat_indv[f3dat_indv$PopA=="36_AMNH_32702",]$Coverage, f3dat_indv[f3dat_indv$PopA=="36_AMNH_32702",]$F3)
-
-f3<- ggplot(f3dat_indv[f3dat_indv$PopA=="36_AMNH_32702",], aes(Coverage, F3))
-f3 <- f3 + geom_point() + geom_smooth(method = "lm")+
-  scale_fill_viridis_c()
-f3
+ggarrange(ggarrange(NULL,f3,NULL, ncol=3, widths = c(1,4,1), 
+                    labels = c("","A","")),
+          ggarrange(p,q,ncol=2, labels=c("B","C")), ncol=1)
+ggsave("Plots/FigureS6.pdf", width = 10, height = 10)
