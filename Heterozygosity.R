318e1f6a30b50843c887688ad567b8fe995759b2
Claudia Fontsere <qvw641@sun1020626.unicph.domain>
2023-03-14 11:29:50 +0100

    Heterozygosity

--- Heterozygosity.R
+++ Heterozygosity.R
@@ -4,71 +4,22 @@ library("ggConvexHull")
 library("scales")
 library("ggrepel")
 library("RColorBrewer")
-library("viridis")
 library("readxl")
+library("ggpubr")
 
 setwd("~/Documents/OneDrive - University of Copenhagen/CottonTop_Tamarins/")
-metadata <- read_excel("Shotgun_Metadata.xlsx", sheet = 1)
-metadata <- metadata[-which(metadata$Full_ID=="37_AMNH_33174"|metadata$Full_ID=="CEI_060"),]
+metadata <- read_excel("Paper/SupplementaryTables_v1.1.xlsx", sheet = 4)
+
+metadata <- metadata[-which(metadata$Full_ID=="CEI_060"),]
 
 colors = c("#645244","#832232","grey","#70a37f","#b3dec1","#fbefa6","#fab2ea","#6d466b",
            "#508991","#0a4a33", "#eb9486","grey")
-# F INBREEDING PLINK -----------
 
-g <- ggplot(metadata, aes(Site, F_plink, fill=Site))
+# F INBREEDING NGSRELATE -----
+
+g <- ggplot(metadata, aes(Site, `Fi (NGSrelate)`, fill=Site))
 g <- g+ geom_boxplot() + geom_jitter()+  facet_grid(.~Type, space="free", scales="free_x") + 
-  theme_classic() +
-  theme(legend.position = "none")
-g
-
-pdf("Plots/Inbreeding_F.pdf", height=4, width=11)
-g
-dev.off()
-
-g <- ggplot(metadata[metadata$Coverage>5,], aes(Site, F_plink, fill=Site))
-g <- g+ geom_boxplot() + geom_jitter()+  facet_grid(.~Type, space="free", scales="free_x") + 
-  theme_classic() +ggtitle("Only Samples > 5x")+
-  theme(legend.position = "none")
-g
-
-pdf("Plots/Inbreeding_F_highCov.pdf", height=4, width=11)
-g
-dev.off()
-
-g <- ggplot(metadata, aes(Coverage, F_plink, color=Site, shape=Type))
-g <- g+ geom_point() +
-  theme_classic() 
-g
-
-pdf("Plots/Inbreeding_FvsCoverage.pdf", height=5, width=6)
-g
-dev.off()
-
-g <- ggplot(metadata, aes(Missingess, F_plink, color=Site, shape=Type))
-g <- g+ geom_point() +
-  theme_classic() + xlab("Missingness")
-g
-
-pdf("Plots/Inbreeding_FvsMissingness.pdf", height=5, width=6)
-g
-dev.off()
-
-
-g <- ggplot(metadata, aes(Missingess, Coverage, color=Site, shape=Type))
-g <- g+ geom_point() +
-  theme_classic() + xlab("Missingness")
-g
-
-pdf("Plots/CoveragevsMissingness.pdf", height=5, width=6)
-g
-dev.off()
-
-
-# F INBREEDING NGSRELATE to do-----
-
-g <- ggplot(metadata[metadata$Full_ID!="CEI_060",], aes(Site, `F NgsRelate`, fill=Site))
-g <- g+ geom_boxplot() + geom_jitter()+  facet_grid(.~Type, space="free", scales="free_x") + 
-  theme_classic() + ylab("Inbreeding (F)")+ scale_fill_manual(values=colors)+
+  theme_classic() + ylab("Inbreeding (Fi)")+ scale_fill_manual(values=colors)+
   theme(legend.position = "none")
 g
 
@@ -76,88 +27,7 @@ pdf("Plots/Inbreeding_F_ngsrelate.pdf", height=4, width=11)
 g
 dev.off()
 
-
-# Heterozygosity with SNPs ----
-
-het <- list.files("Files/Het/scaffolds/", pattern = ".het", full.names = TRUE)
-ldf_het <- lapply(het, read.table)
-
-samplesid <- unlist(lapply(1:length(basename(het)), function(i) strsplit(basename(het)[i],"_t")[[1]][1]))
-
-for (i in 1:length(samplesid)){
-  Full_ID <- samplesid[i]
-  ldf_het[[i]]<- cbind.data.frame(ldf_het[[i]], Full_ID=as.character(Full_ID))
-}
-
-
-df_het <- do.call(rbind.data.frame,ldf_het)
-
-df_het1 <- merge(df_het, metadata[ , 1:12], by="Full_ID")                            
-
-g <- ggplot(df_het1, aes(Full_ID, V4, fill=Type))
-g <- g+ geom_boxplot() + geom_jitter()+  facet_grid(.~Site, space="free", scales="free_x") + 
-  theme_classic() +
-  theme(legend.position = "none")
-g
-
-
-g <- ggplot(df_het1, aes(V1, V4))
-g <- g+ geom_boxplot() + geom_jitter()+   
-  theme_classic() +
-  theme(legend.position = "none")
-g
-
-#Calculate averag per scaffold and per individual
-average_het <- list()
-for (i in 1:length(samplesid)){
-  Full_ID <- samplesid[i]
-  average_het[[i]]<- cbind.data.frame(Full_ID=as.character(Full_ID), 
-                                      MeanHet=mean(ldf_het[[i]]$V4),
-                                      MedianHet=median(ldf_het[[i]]$V4))
-}
-df_avhet <- do.call(rbind.data.frame,average_het)
-
-#save the values in metadata
-
-g <- ggplot(metadata, aes(Site, `Median Het`, fill=Site))
-g <- g+ geom_boxplot(outlier.shape = NA) + geom_jitter()+  facet_grid(.~Type, space="free", scales="free_x") + 
-  theme_classic() + ylab("Median Genome-wide Heterozygosity (bp-1)")+
-  theme(legend.position = "none")
-g
-
-pdf("Plots/Heterozygosity_new.pdf", height=4, width=11)
-g
-dev.off()
-
-g <- ggplot(metadata[metadata$Coverage>5,], aes(Site, `Median Het`, fill=Site))
-g <- g+ geom_boxplot() + geom_jitter()+  facet_grid(.~Type, space="free", scales="free_x") + 
-  theme_classic() + ggtitle("Only Samples > 5x")+
-  theme(legend.position = "none")
-g
-
-pdf("Plots/Heterozygosity_highCov.pdf", height=4, width=11)
-g
-dev.off()
-
-g <- ggplot(metadata, aes(Missingess, `Median Het`, color=Site, shape=Type))
-g <- g+ geom_point() +
-  theme_classic() + xlab("Missingness")
-g
-
-pdf("Plots/HeterozygosityvsMissingness.pdf", height=5, width=6)
-g
-dev.off()
-
-g <- ggplot(metadata, aes(Coverage, `Median Het`, color=Site, shape=Type))
-g <- g+ geom_point() +
-  theme_classic() 
-g
-
-pdf("Plots/HeterozygosityvsCoverage.pdf", height=5, width=6)
-g
-dev.off()
-
-# Het from ANGSD -----
+# HETEROZYGOSITY from ANGSD -----
 fins <- list.files("Files/Het/angsd/",pattern=".ml",full.names=TRUE)
 samples <- sub('\\.ml$', '', basename(fins))
 
@@ -174,8 +44,9 @@ for (i in 1:length(samples)){
 
 het_df <- do.call(rbind,df_list2)
 head(het_df)
+
 #save the values in metadata
-g <- ggplot(metadata, aes(Site, `Angsd Het`, fill=Site))
+g <- ggplot(metadata, aes(Site, `Heterozygosity (ANGSD)`, fill=Site))
 g <- g+ geom_boxplot(outlier.shape = NA) + geom_jitter()+ 
   facet_grid(.~Type, space="free", scales="free_x") + 
   theme_classic() + ylab("Heterozygosity (bp-1)")+
@@ -187,24 +58,24 @@ g
 dev.off()
 
 
-g <- ggplot(metadata, aes(Coverage, `Angsd Het`, color=Site, shape=Type))
-g <- g+ geom_point() +
+cor.test(metadata$Coverage,metadata$`Heterozygosity (ANGSD)`)
+summary(lm(metadata$Coverage~metadata$`Heterozygosity (ANGSD)`))
+
+g <- ggplot(metadata, aes(Coverage, `Heterozygosity (ANGSD)`))
+g <- g+ geom_point() + geom_smooth(method = "lm")+ ylab("Heterozygosity (bp-1)")+
   theme_classic() + scale_color_manual(values=colors)
 g
-pdf("Plots/Heterozygosity_angsd_coverage.pdf", height=4, width=11)
+
+pdf("Plots/Heterozygosity_angsd_coverage.pdf", height=4, width=6)
 g
 dev.off()
 
 ctt <- metadata
-test <- t.test(ctt[ctt$Type=="Historical",]$`Angsd Het`, 
-               ctt[ctt$Type=="Modern",]$`Angsd Het`)
-
+test <- t.test(ctt[ctt$Type=="Historical",]$`Heterozygosity (ANGSD)`, 
+               ctt[ctt$Type=="Modern",]$`Heterozygosity (ANGSD)`)
 test$p.value
-library(ggpubr)
-compare_means(formula, data, method = "wilcox.test", paired = FALSE,
-              group.by = NULL, ref.group = NULL, ...)
 
-g <- ggplot(ctt, aes(Type, `Angsd Het`, fill=Type))
+g <- ggplot(ctt, aes(Type, `Heterozygosity (ANGSD)`, fill=Type))
 g <- g+ geom_boxplot(outlier.shape = NA) + geom_jitter()+ 
   stat_compare_means(label.x = 1.5, label.y = 0.0014)+
   theme_classic() + xlab("") + ylab("Heterozygosity (bp-1)") + 
@@ -215,13 +86,17 @@ pdf("Plots/Heterozygosity_testHistModern.pdf", height=4, width=7)
 g
 dev.off()
 
-mean(ctt[ctt$Group!="Unknown"&ctt$Type=="Historical",]$`Angsd Het`)/mean(ctt[ctt$Group!="Unknown"&ctt$Type=="Modern",]$`Angsd Het`)
-mean(ctt[ctt$Group!="Unknown"&ctt$Type=="Historical"&ctt$Group=="Greater Northeast",]$`Angsd Het`)/mean(ctt[ctt$Group!="Unknown"&ctt$Type=="Modern"&ctt$Group=="Greater Northeast",]$`Angsd Het`)
-mean(ctt[ctt$Group!="Unknown"&ctt$Type=="Historical"&ctt$Group=="Southwest",]$`Angsd Het`)/mean(ctt[ctt$Group!="Unknown"&ctt$Type=="Modern"&ctt$Group=="Southwest",]$`Angsd Het`)
+groups <- c("Southwest", "Greater Northeast")
+mean(ctt[which(ctt$`Grouping (2 Regions)`%in%groups&ctt$Type=="Historical"),]$`Heterozygosity (ANGSD)`)/
+  mean(ctt[which(ctt$`Grouping (2 Regions)`%in%groups&ctt$Type=="Modern"),]$`Heterozygosity (ANGSD)`)
+mean(ctt[ctt$Type=="Historical",]$`Heterozygosity (ANGSD)`)/mean(ctt[ctt$Type=="Modern",]$`Heterozygosity (ANGSD)`)
+
+mean(ctt[ctt$`Grouping (2 Regions)`!="Unknown"&ctt$Type=="Historical"&ctt$`Grouping (2 Regions)`=="Greater Northeast",]$`Heterozygosity (ANGSD)`)/mean(ctt[ctt$`Grouping (2 Regions)`!="Unknown"&ctt$Type=="Modern"&ctt$`Grouping (2 Regions)`=="Greater Northeast",]$`Heterozygosity (ANGSD)`)
+mean(ctt[ctt$`Grouping (2 Regions)`!="Unknown"&ctt$Type=="Historical"&ctt$`Grouping (2 Regions)`=="Southwest",]$`Heterozygosity (ANGSD)`)/mean(ctt[ctt$`Grouping (2 Regions)`!="Unknown"&ctt$Type=="Modern"&ctt$`Grouping (2 Regions)`=="Southwest",]$`Heterozygosity (ANGSD)`)
 
 
-g11 <- ggplot(ctt[ctt$Group!="Unknown",], aes(Type, `Angsd Het`, fill=Type))
-g11 <- g11+ geom_boxplot(outlier.shape = NA) + geom_jitter()+ facet_grid(.~Group)+
+g11 <- ggplot(ctt[which(ctt$`Grouping (2 Regions)`%in%groups),], aes(Type, `Heterozygosity (ANGSD)`, fill=Type))
+g11 <- g11+ geom_boxplot(outlier.shape = NA) + geom_jitter()+ facet_grid(.~`Grouping (2 Regions)`)+
   stat_compare_means(label.x = 1.5, label.y = 0.0014)+
   theme_classic() + xlab("") + ylab("Heterozygosity (bp-1)")+
   scale_fill_manual(values=c("#913C5C","#FEDFFF"))
@@ -233,8 +108,8 @@ g11
 dev.off()
 
 
-g <- ggplot(ctt[ctt$Coverage > 7&ctt$Group!="Unknown",], aes(Type, `Angsd Het`, fill=Type))
-g <- g+ geom_boxplot(outlier.shape = NA) + geom_jitter()+ facet_grid(.~Group)+
+g <- ggplot(ctt[which(ctt$`Grouping (2 Regions)`%in%groups & ctt$Coverage > 7),], aes(Type, `Heterozygosity (ANGSD)`, fill=Type))
+g <- g+ geom_boxplot(outlier.shape = NA) + geom_jitter()+ facet_grid(.~`Grouping (2 Regions)`)+
   stat_compare_means(label.x = 1.5, label.y = 0.0014)+
   theme_classic() + xlab("") + ylab("Heterozygosity (bp-1)")+
   scale_fill_manual(values=c("#913C5C","#FEDFFF"))
@@ -245,23 +120,15 @@ pdf("Plots/Heterozygosity_testHistModern_highcov.pdf", height=4, width=7)
 g
 dev.off()
 
-# RoH for the paper
-
-library(ggplot2)
-library(scales)
-library("readxl")
-library("ggpubr")
-library(tidyr)
-setwd("~/Documents/OneDrive - University of Copenhagen/CottonTop_Tamarins/")
-metadata  <- read_excel("Shotgun_Metadata.xlsx")
-
-metadata_8x <- metadata[metadata$Site!="Saguinus_geoffroyi"&metadata$Coverage>8&metadata$Full_ID!="CEI_060",]
-
+# RUNS OF HOMOGYZOSITY form ROHan -------
+#only samples with coverage >7x with Rohan
+samples <- c("24_FMNH_69286","36_AMNH_32702","39_AMNH_130390","41_FMNH_69938","CEI_051","SJN_001","SJN_011","TLP2_03","TLP9_01")
+metadata_7x <- metadata[which(metadata$Full_ID%in%samples),]
 
 # ROHan summary length RoHs -----
-roh <- list.files("Files/ROHan/", pattern = "_2.5e4_allscaffolds_1Mb.mid.hmmrohl", full.names = TRUE)# This one
+roh <- list.files("Files/Het/rohan/", pattern = "_aDNA_2e5_allscaffolds_1Mb.mid.hmmrohl", full.names = TRUE)
 ldf_het <- lapply(roh, read.table)
-V1 <- gsub("_2.5e4_allscaffolds_1Mb.mid.hmmrohl","", basename(roh)) # This one
+V1 <- gsub("_aDNA_2e5_allscaffolds_1Mb.mid.hmmrohl","", basename(roh)) # This one
 finalV1 <- V1
 
 for (i in 1:length(finalV1)){
@@ -277,12 +144,11 @@ colnames(assembly) <- c("Scaffold","Length","Pos","V4","V5")
 roh_final <- merge(df_roh, assembly[,c("Scaffold","Length"),], by="Scaffold")
 roh_final_rohan <- roh_final
 
-list_roh <- list()
 # per sample  -----
+list_roh<- list()
 for (i in 1:length(finalV1)){  
   sample <- as.character(finalV1[i])
-  df <- roh_final[roh_final$Sample==sample,]
-  df_het_sample <- df_het[df_het$Sample == sample,]
+  df <- roh_final_rohan[roh_final_rohan$Sample==sample,]
   p1 <- ggplot(df)
   p1 <- p1 + facet_wrap(.~Scaffold, ncol=1,strip.position="right")+ ggtitle(sample) + 
     ylab("")+ xlab("Position (bp)")+
@@ -292,43 +158,61 @@ for (i in 1:length(finalV1)){
                        limits = c(0,0.01))+
     geom_rect(data=df,
               mapping=aes(xmin = BEGIN, ymin = 0, xmax = END, ymax = 0.01), 
-              fill="#913C5C", alpha=0.8) + 
+              fill="#fc7f03", alpha=0.8) + 
     geom_rect(data=assembly,mapping = aes(xmin=0, xmax=Length, ymin=0, ymax=0.01), alpha = .3,
               color = "#2e2e2e", fill = NA) +
     theme_classic()+
     theme(panel.grid.major = element_blank(), axis.text.y = element_blank(),panel.grid.minor = element_blank(),
           legend.position = "none", axis.ticks.y = element_blank(), panel.border=element_blank(),
           panel.background = element_blank(),axis.line = element_blank(), 
-          strip.background = element_blank(), strip.text = element_blank())
+          strip.background = element_blank(), strip.text.y = element_text(angle=0, hjust=1))
   print(p1)
   list_roh[[i]] <- p1
-  ggsave(paste0("Plots/RoHs/RoHans_", sample, "2.5e-4_mid_1Mb.pdf"), width = 6, height = 6)
 }
 
-ggarrange(plotlist=list_roh, labels = c("A", "B", "C","D","E"))
-ggsave("Plots/RoH_final_rohan.pdf", height = 15, width = 13)
+
+list_roh_plot <- c(list_roh[4],list_roh[6],list_roh[7:13])
+
+ggarrange(plotlist=list_roh_plot, labels = c("A", "B", "C","D","E", "F","G","H","I"))
+ggsave("Plots/RoH_final_rohan.pdf", height = 15, width = 13) 
+
+# Age of fragments ------
+roh_final_rohan$Full_ID <- roh_final_rohan$Sample
+roh_final_rohan_metadata <- merge(roh_final_rohan, metadata, by="Full_ID")
+roh_final_rohan_metadata$Age <- 100/(2*(roh_final_rohan_metadata$ROH_LENGTH/1000000))*6
+roh_final_rohan_metadata$Year <- 2020-roh_final_rohan_metadata$Age
+
+roh_final_rohan_metadata <- roh_final_rohan_metadata[roh_final_rohan_metadata$Coverage > 7,] 
+
+roh_final_rohan_metadata$LengthMb <- roh_final_rohan_metadata$ROH_LENGTH/1000000
+
+mean(roh_final_rohan_metadata[roh_final_rohan_metadata$Full_ID=="CEI_051",]$Age)
+min(roh_final_rohan_metadata[roh_final_rohan_metadata$Full_ID=="CEI_051",]$Age)
+
+median(roh_final_rohan_metadata[roh_final_rohan_metadata$Full_ID=="CEI_051",]$Age)
+mean(roh_final_rohan_metadata[roh_final_rohan_metadata$Full_ID=="CEI_051",]$ROH_LENGTH)/1000000
 
 
-
-
-# summary ROH
+# summary ROH ------
 samples_new <- read.table("Files/Samples", header = FALSE)
 
 roh_perc_bcftools <- lapply(1:length(samples_new$V1), function(i) {
-  df2 <- roh_final[roh_final$Sample == samples_new$V1[i],]
+  df2 <- roh_final_rohan[roh_final_rohan$Sample == samples_new$V1[i],]
   data.frame(sample=samples_new$V1[i],
              Total=sum(df2$ROH_LENGTH)/2779462738,  
              Average=mean(df2$ROH_LENGTH),
+             Median=median(df2$ROH_LENGTH),
              Intermediate=sum(df2[df2$ROH_LENGTH>=1000000 & df2$ROH_LENGTH<2500000,]$ROH_LENGTH)/2779462738, 
              Intermediate2=sum(df2[df2$ROH_LENGTH>=2500000 & df2$ROH_LENGTH<5000000,]$ROH_LENGTH)/2779462738, 
              Long=sum(df2[df2$ROH_LENGTH>=5000000 & df2$ROH_LENGTH<8000000,]$ROH_LENGTH)/2779462738, 
              ExtraLong=sum(df2[df2$ROH_LENGTH>=8000000,]$ROH_LENGTH)/2779462738, 
              count=length(df2$ROH_LENGTH),
-             sumMb=sum(df2$ROH_LENGTH))
+             sumMb=sum(df2$ROH_LENGTH),
+             maxMb=max(df2$ROH_LENGTH))
 })
 roh_perc_df_bcftools <- do.call(rbind, roh_perc_bcftools)
 
-roh_perc_df_gather <- gather(roh_perc_df_bcftools, key, value, -sample, -Average, -count,-sumMb)
+roh_perc_df_gather <- gather(roh_perc_df_bcftools, key, value, -sample, -Median,-Average, -count,-sumMb,-maxMb)
 roh_perc_df_gather$key <- gsub("Intermediate2","1-2.5Mb", roh_perc_df_gather$key )
 roh_perc_df_gather$key <- gsub("Intermediate","2.5-5Mb", roh_perc_df_gather$key )
 roh_perc_df_gather$key <- gsub("ExtraLong",">8Mb", roh_perc_df_gather$key )
@@ -342,23 +226,23 @@ roh_perc_df_gather2 <- merge(roh_perc_df_gather, metadata, by="Full_ID")
 
 
 
-roh_perc_df_gather3 <- roh_perc_df_gather2[which(roh_perc_df_gather2$Coverage>8&
+roh_perc_df_gather3 <- roh_perc_df_gather2[which(roh_perc_df_gather2$Coverage>7&
                                                    roh_perc_df_gather2$key!="Total"&
                                                    roh_perc_df_gather2$key!="Average"&
                                                    roh_perc_df_gather2$sample!="CEI_060"),]
 
-pa <- ggplot(roh_perc_df_gather3[roh_perc_df_gather3$value>0,], 
+pa <- ggplot(roh_perc_df_gather3, 
              aes(sample,value*100,fill=key))
-pa <- pa + geom_col()+ theme_classic() +  xlab("")+ facet_grid(.~Group, space = "free", scales="free")+
+pa <- pa + geom_col()+ theme_classic() +  xlab("")+ facet_grid(.~Type,  scales="free")+
   ylab("% of Genome in RoH") + 
-  theme(legend.position = "right", axis.text.y = element_text(hjust=0.5)) + 
+  theme(legend.position = "right", axis.text.y = element_text(hjust=0.5),
+        axis.text.x = element_text(angle=45, hjust=1)) + 
   scale_fill_manual(values=rev(c("#fcd021","#234f96","#5e97f2","#b6d1fc")), name="RoH Size") 
 pa
 
 pdf("Plots/Summary_rohs.pdf", width = 6, height = 5)
-p
+pa
 dev.off()
-
 
 # Final plot for figure 3
 ggarrange(g11,pa,nrow = 2, align = "v",labels=c("A","B"))
